92fe9d9ef23e84202be28c5a6b24371ba11d1405
alexisaldana1 <18729650-alexisaldana1@users.noreply.replit.com>
2023-05-24 21:27:25 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let optionOne = document.querySelector(".option-one")
+optionOne.onclick=function(){ 
+  console.log('in click function')
+  screenOne.style.display = "block"
+}
+
+let optionTwo = document.querySelector(".option-two")
+optionTwo.onclick=function(){ 
+  console.log('in click function')
+  screenTwo.style.display = "block"
+}
+
+let optionThree = document.querySelector(".option-three")
+optionThree.onclick=function(){ 
+  console.log('in click function')
+  screenThree.style.display = "block"
+}
+
+let optionFour = document.querySelector(".option-four")
+optionFour.onclick=function(){ 
+  console.log('in click function')
+  screenFour.style.display = "block"
+}
+
+let optionFive= document.querySelector(".option-five")
+optionFive.onclick=function(){ 
+  console.log('in click function')
+  screenFive.style.display = "block"
+}
+
+let optionSix = document.querySelector(".option-six")
+optionSix.onclick=function(){ 
+  console.log('in click function')
+  screenSix.style.display = "block"
+}
